d5ef37b81b1917b50e4ea3e45247e8a6cd798482
tzainten <122052876+tzainten@users.noreply.github.com>
2023-05-28 12:06:54 -0400

    Testing file transfers between two machines now

---
+++ NetworkStreamHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgnitionLauncher;
+
+public static class NetworkStreamHelper
+{
+    public static void WriteData( TcpClient client, byte[] data )
+    {
+        byte[] eos = Encoding.UTF8.GetBytes( "\0" );
+        byte[] dataWithEos = data.Concat( eos ).ToArray();
+
+        var stream = client.GetStream();
+        stream.Write( dataWithEos );
+    }
+
+    public static bool ReadData( TcpClient client, ref byte[] buffer )
+    {
+        var stream = client.GetStream();
+
+        if ( !stream.CanRead )
+            return false;
+
+        string data = string.Empty;
+
+        try
+        {
+            int byteCount;
+            while ( ( byteCount = stream.Read( buffer, 0, buffer.Length ) ) != 0 )
+            {
+                Console.WriteLine( byteCount );
+
+                data += Encoding.UTF8.GetString( buffer, 0, byteCount );
+                if ( data.EndsWith( "\0" ) )
+                    return true;
+            }
+
+            return false;
+        }
+        catch ( Exception ex )
+        {
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace IgnitionLauncher;
 
@@ -13,15 +15,117 @@ public class Program
     static Client? Client;
     static Server? Server;
 
+    //static string PackagedContentRoot => $"C:\\Users\\Jaiden\\Documents\\Visual Studio 2022 Projects\\TestAppForLauncher\\bin\\Debug\\net7.0";
+    //static string ClientContentRoot => $"C:\\Users\\Jaiden\\Documents\\Visual Studio 2022 Projects\\TestAppForLauncher\\ClientContent";
+    static string PackagedContentRoot => $"{Environment.GetFolderPath( Environment.SpecialFolder.Desktop )}/Rogue";
+    static string ClientContentRoot => $"{Environment.GetFolderPath( Environment.SpecialFolder.Desktop )}/Rogue";
+
     public static void Main( string[] args )
     {
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-#if CLIENT
-        Client = new();
-#else
-        Server = new();
-#endif
+        string type = string.Empty;
+
+        if ( ( type = Console.ReadLine() ) == "1" )
+        {
+            TcpListener listener = new( IPAddress.Any, 11000 );
+            listener.Start();
+
+            TcpClient client;
+            while ( true )
+            {
+                client = null;
+
+                try
+                {
+                    client = listener.AcceptTcpClient();
+                    var stream = client.GetStream();
+
+                    byte[] buffer = new byte[ client.ReceiveBufferSize ];
+
+                    int byteCount = stream.Read( buffer, 0, buffer.Length );
+
+                    var readableData = Encoding.UTF8.GetString( buffer, 0, byteCount ).Split( "@" );
+
+                    foreach ( var item in Directory.GetFiles( PackagedContentRoot, "*", SearchOption.AllDirectories ) )
+                    {
+                        if ( !item.Contains( readableData[ 0 ] ) ) continue;
+
+                        var file = File.ReadAllBytes( item );
+                        var filemd5 = BuildHandler.GetMD5String( BuildHandler.GetMD5Hash( file ) );
+
+                        if ( filemd5 != readableData[ 1 ] )
+                        {
+                            byte[] fileName = Encoding.UTF8.GetBytes( $"{item.Replace( $"{PackagedContentRoot}\\", string.Empty )}@" );
+                            byte[] combinedData = fileName.Concat( file ).ToArray();
+
+                            stream.Write( combinedData, 0, file.Length );
+                            break;
+                        }
+                    }
+
+                    stream.Close();
+                    client.Close();
+
+                    Console.WriteLine( $"data: {readableData[ 0 ]}, {readableData[ 1 ]}" );
+                }
+                catch ( Exception ex ) { }
+            }
+        }
+        else
+        {
+            foreach ( var item in Directory.GetFiles( ClientContentRoot, "*", SearchOption.AllDirectories ) )
+            {
+                var file = File.ReadAllBytes( item );
+                var filemd5 = BuildHandler.GetMD5String( BuildHandler.GetMD5Hash( file ) );
+
+                try
+                {
+                    TcpClient client = new();
+                    client.Connect( "192.168.1.246", 11000 );
+
+                    var stream = client.GetStream();
+
+                    byte[] data = Encoding.UTF8.GetBytes( $"{item.Replace( $"{ClientContentRoot}\\", string.Empty )}@{filemd5}" );
+
+                    stream.Write( data, 0, data.Length );
+
+                    byte[] buffer = new byte[ client.ReceiveBufferSize ];
+                    int byteCount = stream.Read( buffer, 0, buffer.Length );
+                    if ( byteCount <= 0 ) continue;
+
+                    var dataAsString = Encoding.UTF8.GetString( buffer, 0, byteCount );
+
+                    int splitIndex = 0;
+                    string fileName = string.Empty;
+                    for ( int i = 0; i < dataAsString.Length; i++ )
+                    {
+                        splitIndex++;
+
+                        char c = dataAsString[ i ];
+                        if ( c == '@' ) break;
+
+                        fileName += dataAsString[ i ];
+                    }
+
+                    byte[] dataWithoutFileName = buffer.Skip( splitIndex ).ToArray();
+
+                    File.WriteAllBytes( $"{ClientContentRoot}/{fileName}", dataWithoutFileName );
+
+                    //Console.WriteLine( fileName );
+
+                    //Console.WriteLine( $"data: {Encoding.UTF8.GetString( buffer, 0, byteCount )}" );
+
+                    client.Close();
+                }
+                catch ( Exception ex )
+                {
+                    Console.WriteLine( ex.Message );
+                }
+            }
+
+            Console.ReadKey();
+        }
 
         /*BuildHandler.DetermineMostRecentBuild();
 
